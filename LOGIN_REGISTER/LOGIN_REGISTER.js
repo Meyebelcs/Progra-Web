47f6dc5d08d8c137f8ad2f5e45439116230904be
DenisseCardozaP <denisse_cardoza@hotmail.com>
2022-09-10 00:27:54 -0500

    Control de errores
    Ya marca si se ingresó correctamente la información, de lo contrario muestra un mensaje de error.

--- LOGIN_REGISTER/LOGIN_REGISTER.js
+++ LOGIN_REGISTER/LOGIN_REGISTER.js
@@ -1,7 +1,7 @@
 document.getElementById("btn_registrarse").addEventListener("click",register);
 document.getElementById("btn_inciar-sesión").addEventListener("click",iniciarSesion);
-document.getElementById("login").addEventListener("click",entrar);
-document.getElementById("registerf").addEventListener("click", registerf);
+/* document.getElementById("login").addEventListener("click",entrar);*/
+document.getElementById("registerf").addEventListener("click", registerf); 
 
 let contenedorLoginRegister=document.querySelector(".contenedor_login-register");
 let formularioLogin=document.querySelector(".formulario_login");
@@ -9,10 +9,69 @@ let formularioRegister=document.querySelector(".formulario_register");
 let cajaTraseraLogin=document.querySelector(".caja_trasera-login");
 let cajaTraseraRegister=document.querySelector(".caja_trasera-register");
 
+const Nombres= document.getElementById("nombres");
+const Apellidos= document.getElementById("apellidos");
+const Correo= document.getElementById("Correo");
+const Usuario= document.getElementById("Usuario");
+const Contraseña= document.getElementById("Contraseña");
+const Contraseña2= document.getElementById("ConfirmarContraseña");
+const Foto= document.getElementById("foto");
+const form= document.getElementById("register");
+const parrafo= document.getElementById("warnings");
+
+function registerf(){
+
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        if(Nombres.value.length<1){
+           setErrorFor(Nombres,"El nombre no puede estar vacío");
+        }else{
+            setSuccesFor(Nombres);
+        }
+        if(Apellidos.value.length<1){
+            setErrorFor(Apellidos,"El apellido no puede estar vacío");
+         }else{
+            setSuccesFor(Apellidos);
+         }
+         if(Foto.value.length<1){
+            setErrorFor(Foto,"No ha agregado ninguna foto");
+         }else{
+            setSuccesFor(Foto);
+         }
+    
+    })
+
+}
+
+function setErrorFor(input, message){
+    const formControl=input.parentElement;//.form-control
+    const small=formControl.querySelector('small');
+
+    if(input=Foto){
+        //add error message inside mall
+        small.innerText=message;
+
+        //add error class
+        formControl.className='form-control error';
+    }else{
+        //add error message inside mall
+        small.innerText=message;
+
+        //add error class
+        formControl.className='form-control error';
+    }
+}
+
+function setSuccesFor(input){
+    const formControl=input.parentElement;
+    formControl.className='form-control success';
+}
+
+
 function iniciarSesion(){
     
     formularioRegister.style.display="none";
-    contenedorLoginRegister.style.top="-650px";
+    contenedorLoginRegister.style.top="-530px";
     formularioLogin.style.display="block";
     cajaTraseraRegister.style.opacity="1";
     cajaTraseraLogin.style.opacity="0";
@@ -21,19 +80,20 @@ function iniciarSesion(){
 function register(){
 
     formularioRegister.style.display="block";
-    contenedorLoginRegister.style.top="-150px";
+    contenedorLoginRegister.style.top="5px";
     formularioLogin.style.display="none";
     cajaTraseraRegister.style.opacity="0";
     cajaTraseraLogin.style.opacity="1";
 }
 
-function registerf(){
+/* function registerf(){
    const Nombres= document.getElementById("nombres");
    const Apellidos= document.getElementById("apellidos");
    const Correo= document.getElementById("Correo");
    const Usuario= document.getElementById("Usuario");
    const Contraseña= document.getElementById("Contraseña");
-   const form= document.getElementById("form");
+   const Contraseña2= document.getElementById("ConfirmarContraseña");
+   const form= document.getElementById("register");
    const parrafo= document.getElementById("warnings");
 
     form.addEventListener("submit", es=>{
@@ -97,7 +157,7 @@ function entrar(){
     //if(user=="Mel" && pass == "1234" || user=="Denisse" && pass== "1234"){
     //    window.location="good.html";
     //}
-}
+} */
 
 
 /*
