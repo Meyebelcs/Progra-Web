16b1b3e33930e7d71db681b344052e859876a231
DenisseCardozaP <denisse_cardoza@hotmail.com>
2022-09-13 15:03:08 -0500

    NavBar y Logo
    Se agregó el diseño de la barra de navegación y el logo

--- LOGIN_REGISTER/LOGIN_REGISTER.js
+++ LOGIN_REGISTER/LOGIN_REGISTER.js
@@ -143,7 +143,7 @@ function registerf(){
           }
           
           if(Correonew === '') {
-            setErrorFor(Correo, 'No puede dejar el email en blanco');
+            setErrorFor(Correo, 'El email no puede estar vacío');
           } else if (!isEmail(Correonew)) {
             setErrorFor(Correo, 'No ingreso un email válido');
           } else {
@@ -191,10 +191,9 @@ function registerf(){
          
          //si llega a este punto logró todas las validaciones correctamente
          //Entonces guardará la informacion
-        // if(num===8){
-          //  alert("Se registró correctamente");
-            //return;
-         //}
+        /* if(num===8){
+          alert("Se registró correctamente");
+         } */
          
 
     })
@@ -284,7 +283,7 @@ function setSuccesFor(input){
 function iniciarSesion(){
     
     formularioRegister.style.display="none";
-    contenedorLoginRegister.style.top="-530px";
+    contenedorLoginRegister.style.top="-450px";
     formularioLogin.style.display="block";
     cajaTraseraRegister.style.opacity="1";
     cajaTraseraLogin.style.opacity="0";
@@ -293,91 +292,8 @@ function iniciarSesion(){
 function register(){
 
     formularioRegister.style.display="block";
-    contenedorLoginRegister.style.top="5px";
+    contenedorLoginRegister.style.top="85px";
     formularioLogin.style.display="none";
     cajaTraseraRegister.style.opacity="0";
     cajaTraseraLogin.style.opacity="1";
 }
-
-/* function registerf(){
-   const Nombres= document.getElementById("nombres");
-   const Apellidos= document.getElementById("apellidos");
-   const Correo= document.getElementById("Correo");
-   const Usuario= document.getElementById("Usuario");
-   const Contraseña= document.getElementById("Contraseña");
-   const Contraseña2= document.getElementById("ConfirmarContraseña");
-   const form= document.getElementById("register");
-   const parrafo= document.getElementById("warnings");
-
-    form.addEventListener("submit", es=>{
-    entrar.preventDefault()
-    var warnings=""
-    var entrar=false
-    var regexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    parrafo.innerHTML="" //Lo reinicio
-
-    if(Usuario.value.length <6){
-       warnings += 'El nombre no es valido <br>'
-       entrar = true
-    }
-    if(!regexEmail.test(Correo.value)){ //evalua que haya texto, @, texto y luego .com
-        warnings += 'El email no es valido <br>'
-        entrar = true
-    }
-    if(Contraseña.value.length<8){
-        warnings += 'La contraseña no es valida <br>'
-        entrar = true
-    }
-    if(entrar){
-        parrafo.innerHTML=warnings
-    }else{
-        parrafo.innerHTML= "Enviado"
-    }
-   })
-
-}
-
-function entrar(){
-    alert("entró")
-   
-   const user= document.getElementById("Usernamelogin");
-   const pass= document.getElementById("passwordLogin");
-   const form= document.getElementById("form");
-   const parrafo= document.getElementById("warnings");
-
-    form.addEventListener("submit", es=>{
-        alert("entró")
-    entrar.preventDefault()
-    var warnings=""
-    var entrar=false
-    parrafo.innerHTML="" //Lo reinicio
-
-    if(user.value.length <6){
-       warnings += 'El nombre no es valido <br>'
-       entrar = true
-    }
-    if(pass.value.length<8){
-        warnings += 'La contraseña no es valida <br>'
-        entrar = true
-    }
-    if(entrar){
-        parrafo.innerHTML=warnings
-    }else{
-        parrafo.innerHTML= "Enviado"
-    }
-   })
-
-    //if(user=="Mel" && pass == "1234" || user=="Denisse" && pass== "1234"){
-    //    window.location="good.html";
-    //}
-} */
-
-
-/*
-form.addEventListener("submit", es=>{
-    entrar.preventDefault()
-    if(nombre.value.length <6){
-        alert("Nombre muy corto")
-    }
-   })
-*/ 
