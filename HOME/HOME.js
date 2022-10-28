a1cfa028325b64d989011940cce09dd4316122e2
Meyebelcs <arleth.melany@hotmail.com>
2022-10-28 01:26:14 -0500

    Javascript BG

--- HOME/HOME.js
+++ HOME/HOME.js
@@ -1,6 +1,18 @@
-//SIDEBAR
+//-------------------------SIDEBAR--------------------------
 const menuItems = document.querySelectorAll('.menu-item');
 
+//-------------------------NIGHT MODE--------------------------
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+
+//BACKGROUND
+
+const Bg1= document.querySelector('.bg-1');
+const Bg2= document.querySelector('.bg-2');
+const Bg3= document.querySelector('.bg-3');
+
+
+//=======================SIDEBAR============================
 //remove active class from all menu items
 const changeActiveItem = () => {
     menuItems.forEach(item =>{
@@ -22,3 +34,70 @@ menuItems.forEach(item =>{
         }
     })
 })
+
+//=======================NIGHT MODE============================
+
+//opens modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+//close modal
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none';
+    }
+}
+
+
+themeModal.addEventListener('click', closeThemeModal);
+
+theme.addEventListener('click', openThemeModal);
+
+//=======================BACKGROUND============================
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+//changes background color
+const changeBG = () =>{
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+
+Bg1.addEventListener('click', () =>{
+    //add active class
+    Bg1.classList.add('active');
+    //remove active class from the others
+    Bg2.classList.remove('active');
+    Bg3.classList.remove('active');
+    //remove customized changes from local storage
+    window.location.reload();
+});
+
+Bg2.addEventListener('click', () =>{
+    darkColorLightness ='95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    //add active class
+    Bg2.classList.add('active');
+    //remove active class from the others
+    Bg1.classList.remove('active');
+    Bg3.classList.remove('active');
+    changeBG();
+});
+
+Bg3.addEventListener('click', () =>{
+    darkColorLightness ='95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+    //add active class
+    Bg3.classList.add('active');
+    //remove active class from the others
+    Bg1.classList.remove('active');
+    Bg2.classList.remove('active');
+    changeBG();
+});
